d322f52cf19e025392b5cda76ca8b7c51e3aec98
sanghoone <sanghoone.jung89@gmail.com>
2021-01-14 19:39:05 +0900

    # global style 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { GlobalStyles } from "./global-styles";
+import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <GlobalStyles />
+      <App />
+    </>
   </React.StrictMode>,
   document.getElementById("root")
 );
