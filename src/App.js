c02285c23f6d8e7d72dc77bc6a9515b06d13ed77
sanghoone <sanghoone.jung89@gmail.com>
2021-01-22 19:07:20 +0900

    # router 작업

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ export default function App() {
   return (
     <>
       <Router>
-        <Route exact path="/user">
-          <p>Hello I am a User</p>
+        <Route exact path={ROUTES.BROWSE}>
+          <Browse />
+        </Route>
+        <Route exact path={ROUTES.SIGN_IN}>
+          <Signin />
+        </Route>
+        <Route exact path={ROUTES.SIGN_UP}>
+          <Signup />
         </Route>
         <Route exact path={ROUTES.HOME}>
           <Home />
